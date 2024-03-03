48f30f5081945821386ec73bbdbc3694f01c7e91
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-03-04 00:05:24 +0100

    Included Python Projects

--- update-subtrees.sh
+++ update-subtrees.sh
@@ -4,3 +4,5 @@ git subtree pull --prefix=C++/Cpp-Simple-Examples git@github.com:kryczkal/Cpp-Si
 git subtree pull --prefix=C++/Hanoi git@github.com:kryczkal/Hanoi main --squash
 git subtree pull --prefix=C-Posix-Operating-Systems git@github.com:kryczkal/C-Posix-Operating-Systems main --squash
 git subtree pull --prefix=Matlab-Numerical-Recipes git@github.com:kryczkal/Matlab-Numerical-Recipes main --squash
+git subtree pull --prefix=Python/SnakeAI git@github.com:kryczkal/SnakeAI main --squash
+git subtree pull --prefix=Python/TalkTales git@github.com:kryczkal/TalkTales main --squash
