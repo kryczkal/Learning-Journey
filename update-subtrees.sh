367fd2c64696b98b2935448c9aeaaacf7f099ac8
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-03-03 21:11:20 +0100

    Add update-subtrees.sh script to pull latest changes from external repositories

---
+++ update-subtrees.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git subtree pull --prefix=C#/CSharp-Simple-Libraries git@github.com:kryczkal/CSharp-Simple-Libraries main --squash
+git subtree pull --prefix=C++/Cpp-Simple-Examples git@github.com:kryczkal/Cpp-Simple-Examples main --squash
+git subtree pull --prefix=C++/Hanoi git@github.com:kryczkal/Hanoi main --squash
+git subtree pull --prefix=C-Posix-Operating-Systems git@github.com:kryczkal/C-Posix-Operating-Systems main --squash
+git subtree pull --prefix=Matlab-Numerical-Recipes git@github.com:kryczkal/Matlab-Numerical-Recipes main --squash
